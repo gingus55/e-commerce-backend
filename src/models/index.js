8ce9d150a077a89af8c2cda11a6ef22c64d6d696
gingus55 <gingus55@hotmail.com>
2021-12-15 16:59:34 +0000

    association started

--- src/models/index.js
+++ src/models/index.js
@@ -1,12 +1,18 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "",
+});
 
 // Products belongToMany Tags (through ProductTag)
 
